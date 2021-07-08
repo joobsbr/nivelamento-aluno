49023f22cef4c299db3de22a8f15eb8128fda9f9
joobsbr <rafael.oliveiradasilva@outlook.com.br>
2021-07-08 15:36:33 -0300

    01-exercicio.rb

--- 01-exercicio.rb
+++ 01-exercicio.rb
@@ -1,14 +1,60 @@
-Exercicio 01:
-Escreva um programa que calcula e exiba na tela:
-- quantas horas há em um ano?
-- quantos minutos há em uma década?
-- qual é a sua idade em segundos?
-- quantos chocolates você pretende comer na vida?
-Desafio:
-- Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?
+puts "\n"
+puts "Quantas horas há em um ano?"
+
+puts "\n"
+
+puts "1 ano = 365 dias " 
+puts "1 dia = 24 horas"
+puts "365 dias = x horas"
+puts "x horas = 365 * 24 = " + (365 * 24 ).to_s
+
+puts "\n"
+
+puts "Quantos minutos há em uma décadas?"
+
+puts "\n"
+
+puts "1 dia tem 24 horas "
+puts "1 hora tem 60 minutos"
+puts "24 horas tem 1.440 Minutos " "ou " "24 * 60 = " + (60 * 24).to_s
+puts "1 ano ou 365 dias  tem 525.600 minutos" "ou " "1440 * 365= " + (1440 * 365).to_s
+puts "1 decada tem 5.256.000 minutos" "ou" "525.600 * 10 = " + (525600 * 10).to_s 
 
 
+puts "\n"
 
-teste 42
+puts "Qual é a sua idade em segundos?"
 
+puts "\n"
+
+puts "1 dia = 24 horas"
+puts "1 mes = 30 * 24h = 720h no mês"
+puts "1 ano tem 12 meses ou seja 720h x 12 meses = 8640h"
+puts "Minha idade em horas é de 8640h"
+puts "Minha idade é 25 anos"
+puts "8640 x 25" + (8640 * 25).to_s
+puts "portanto com 25 anos ja vivi 25.216.000h" 
+
+puts "\n"
+
+puts "quantos chocolates você pretende comer na vida?"
+
+puts "\n"
+
+puts "Não como chocolate pois tenho Labirintite"
+
+puts "Porem gosto muito de bala"
+
+puts "\n"
+puts "Minha expectativa de vida é de 80 anos"
+puts "Como em media 3 balas ao dia"
+puts "Como 90 balas em 1 mês"
+
+puts "\n"
+
+puts  "Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
+
+puts "tenho 1.232.000.000 segundos de vida"
+puts "Tenho 20.533.333 minutos de vida"
+puts "Tenho "
 
