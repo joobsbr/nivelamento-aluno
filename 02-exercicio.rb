e0bedb76c0644f7095b6bdbad6ea2084e4cf95fb
joobsbr <rafael.oliveiradasilva@outlook.com.br>
2021-07-08 18:20:19 -0300

    Exercicio 2

--- 02-exercicio.rb
+++ 02-exercicio.rb
@@ -1,13 +1,15 @@
 puts "\n"
+
 puts "Quantas horas há em um ano?"
 
 puts "\n"
 
-puts 1 * 365 * 24
-
 a1 = 365
 a2 = 24
-puts a1 * a2
+
+resultado = a1 * a2
+
+puts resultado
 
 puts "\n"
 
@@ -15,15 +17,15 @@ puts "Quantos minutos há em uma décadas?"
 
 puts "\n"
 
-puts 1 * 10 * 365 * 24 * 60
-
 a1 = 1
 a2 = 10
 a3 = 365
 a4 = 24
 a5 = 60
 
-puts a1 * a2 * a3 * a4 * a5
+resultado = a1 * a2 * a3 *a4 * a5
+
+puts resultado
 
 
 puts "\n"
@@ -32,7 +34,6 @@ puts "Qual é a sua idade em segundos?"
 
 puts "\n"
 
-puts 25 * 365 * 24 * 60 * 60
 
 a1 = 25
 a2 = 365
@@ -40,35 +41,45 @@ a3 = 24
 a4 = 60
 a5 = 60
 
-puts a1 * a2 * a3 * a4 * a5
+resultado = a1 * a2 * a3 * a4 * a5 
+
+puts resultado
 
 puts "\n"
 
-puts "quantos chocolates você pretende comer na vida?"
+puts "quantas balas você pretende comer na vida?"
 
 puts "\n"
-
-puts "Não como chocolate pois tenho Labirintite"
 
 puts "Porem gosto muito de bala"
 
-puts 3 * 12 * 80
+puts "\n" 
 
 a1 = 3
 a2 = 12
 a3 = 80
 
-puts a1 * a2 * a3
+resultado = a1 * a2 * a3
+
+puts resultado
 
 puts "\n"
 
 puts  "Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
 
-puts 1232000000 / 60 / 60 / 24 / 365
-puts 1232000000.0 / 60 / 60 / 24 / 365
+puts "\n"
+
+
+
 
 a1 = 1232000000
 a2 = 60
 a3 = 60
 a4 = 24
 a5 = 365
+
+
+idade = a1.to_f / a2 / a3 / a4 / a5
+
+
+puts idade
