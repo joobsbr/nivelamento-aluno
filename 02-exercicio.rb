83dd13a2b1788e6c02839b12617a7fba292090f0
joobsbr <rafael.oliveiradasilva@outlook.com.br>
2021-07-08 17:02:34 -0300

    02-exercicio.rb

--- 02-exercicio.rb
+++ 02-exercicio.rb
@@ -5,10 +5,9 @@ puts "\n"
 
 puts 1 * 365 * 24
 
-um_dia = 24
-ano = 365
-
-puts um_dia * ano
+a1 = 365
+a2 = 24
+puts a1 * a2
 
 puts "\n"
 
@@ -18,6 +17,14 @@ puts "\n"
 
 puts 1 * 10 * 365 * 24 * 60
 
+a1 = 1
+a2 = 10
+a3 = 365
+a4 = 24
+a5 = 60
+
+puts a1 * a2 * a3 * a4 * a5
+
 
 puts "\n"
 
@@ -26,6 +33,14 @@ puts "Qual é a sua idade em segundos?"
 puts "\n"
 
 puts 25 * 365 * 24 * 60 * 60
+
+a1 = 25
+a2 = 365
+a3 = 24
+a4 = 60
+a5 = 60
+
+puts a1 * a2 * a3 * a4 * a5
 
 puts "\n"
 
@@ -37,7 +52,13 @@ puts "Não como chocolate pois tenho Labirintite"
 
 puts "Porem gosto muito de bala"
 
-puts 3 * 12 * 200
+puts 3 * 12 * 80
+
+a1 = 3
+a2 = 12
+a3 = 80
+
+puts a1 * a2 * a3
 
 puts "\n"
 
@@ -45,3 +66,9 @@ puts  "Se minha idade é de 1232 milhões de segundos, qual é minha idade em an
 
 puts 1232000000 / 60 / 60 / 24 / 365
 puts 1232000000.0 / 60 / 60 / 24 / 365
+
+a1 = 1232000000
+a2 = 60
+a3 = 60
+a4 = 24
+a5 = 365
