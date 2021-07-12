f779a03c7ded46a583c0ba0a0989828f3d2f2d1e
joobsbr <rafael.oliveiradasilva@outlook.com.br>
2021-07-12 17:32:09 -0300

    quizz

---
+++ exercicio-quizz.rb
@@ -0,0 +1,28 @@
+def a()
+    b()
+    c()
+    f()
+end
+
+def b()
+    c()
+    d()
+end
+def c()
+    print 'c'
+end
+def d()
+    c()
+    e()
+end
+def e()
+    print 'e'
+end
+def f()
+    print 'f'
+end
+def main()
+    a()
+end
+
+main()
