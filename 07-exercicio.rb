93549ad31a1dca5c8e3801be2beb74be412bb933
joobsbr <rafael.oliveiradasilva@outlook.com.br>
2021-07-14 18:03:05 -0300

    exercicio

--- 07-exercicio.rb
+++ 07-exercicio.rb
@@ -3,6 +3,11 @@
 # Por exemplo, se o chute for um número fora do intervalo entre 0 e 100, ou se não for um valor numérico, o programa deve imprimir na tela uma mensagem de que o chute é inválido.
 # Subir no GitHub como 07-exercício. (editado) 
 
+
+puts "Olá vamos jogar ?"
+puts "Entre 0 a 100 pense em um numero !"
+
+
 numero = rand(0..100)
 puts "(" + numero.to_s + ")"
 
