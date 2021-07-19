6fa88bcf3a15cc0e27387f2d872472a81f45c64a
joobsbr <rafael.oliveiradasilva@outlook.com.br>
2021-07-19 18:01:07 -0300

    exercicio

--- 12-exercicio.rb
+++ 12-exercicio.rb
@@ -1,3 +1,9 @@
+# Exercicio 12
+# Fazer uma função chamada 'insere_no_array' que recebe um array, um valor qualquer e a posição alvo como parâmetros e que deve inserir o valor informado na posição alvo, de modo a 'empurrar para a frente' os demais elementos e ao final retornar true. Se a posição alvo não estiver no limite do array, a função deve retornar false.
+# Enviar como 12-exercicio.rb
+
+
+
 def insere_array(array, valor, posicao_alvo)
     if posicao_alvo < array.size
         n = array.size
