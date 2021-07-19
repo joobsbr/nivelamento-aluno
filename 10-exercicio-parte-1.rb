1ee66017a53e703e9674f5c2a9bfda925318aed2
joobsbr <rafael.oliveiradasilva@outlook.com.br>
2021-07-19 09:51:34 -0300

    exercicio

--- 10-exercicio-parte-1.rb
+++ 10-exercicio-parte-1.rb
@@ -2,16 +2,16 @@
 # 1) Desenhe um quadrado de 10 linhas por 10 colunas. Desenhar as bordas com “*”, preencher com “x”.
 # Enviar 10-exercicio-parte-1.rb
 
-    for x in (1..10)
-    print "*"
-        for y in (1..8)
-        print "x"
-        end
-            for y in (10..10)
+ for linha in (1..10)
+    for coluna in (1..10)
+        if linha == 1 || linha == 10 || coluna == 1 || coluna == 10
             print "*"
-            end
-            puts
+        else
+            print "x"
+        end
     end
+    puts
+end
    
 
                 
