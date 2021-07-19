64e32e32360e48e320a7736f97e9f03b9006d44b
joobsbr <rafael.oliveiradasilva@outlook.com.br>
2021-07-18 21:13:59 -0300

    exercicio

--- testeif2.rb
+++
@@ -1,7 +0,0 @@
-
-numero = 10
-if numero == 10
-    puts "numero é igual a 10"
-elsif numero != 5
-    puts "numero é diferente de 5"
-end
--- testeround.rb
+++
@@ -1,8 +0,0 @@
-# exemplo de funçoes que manipulam valores numericos
-
-numero = 1.6
-
-puts numero
-puts numero.round(2)
-puts numero.round(0)
-puts numero.truncate(2)
