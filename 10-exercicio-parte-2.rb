85e0104aa371bb63004631307e595e99f49f5221
joobsbr <rafael.oliveiradasilva@outlook.com.br>
2021-07-19 10:10:37 -0300

    exercicio

--- 10-exercicio-parte-2.rb
+++ 10-exercicio-parte-2.rb
@@ -12,25 +12,15 @@
 
 
 
-for x in (1..1)
-    print "*"
-    puts
-    print "**"
-    puts
-    print "***"
-    puts
-    print "****"
-    puts
-    print "*****"
-    puts
-    print "******"
-    puts
-    print "*******"
-    puts
-    print "********"
-    puts
-    print "*********"
-    puts
-    print "**********"
-    puts
+def imprime_triangulo(base)
+    for linha in (1..base)
+        for coluna in (1..linha)
+            print "*"
+        end
+        puts
+    end    
 end
+
+imprime_triangulo(10)
+
+imprime_triangulo(15)
